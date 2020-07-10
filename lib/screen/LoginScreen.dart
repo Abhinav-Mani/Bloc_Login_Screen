02f6cea852b2759833eb61b9d76f8262e3db4614
Abhinav Mani <abhinav1946@gmail.com>
2020-07-10 14:10:26 +0530

    Provider

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../bloc/bloc.dart';
+import '../bloc/Provider.dart';
 
 class LoginScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of(context);
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Column(
         children: [
-          emailField(),
-          passwordField(),
+          emailField(bloc),
+          passwordField(bloc),
           Container(margin:EdgeInsets.only(top:25.0) ),
           submitButton()
         ],
       ),
     );
   }
-  Widget emailField(){
+  Widget emailField(Bloc bloc){
     return StreamBuilder(
       stream: bloc.email,
       builder: (context,snapshot){
@@ -33,7 +35,7 @@ class LoginScreen extends StatelessWidget{
       },
     );
   }
-  Widget passwordField(){
+  Widget passwordField(Bloc bloc){
     return StreamBuilder(
       stream: bloc.password,
       builder: (context,snapshot){
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import '../screen/LoginScreen.dart';
+import '../bloc/Provider.dart';
+
 class App extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen(),
-      ),
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ) ,
     );
   }
 }
